0d5aa2649a99781ca3891fbbe834d14893497749
Leopoldo Méndez Pérez <lmendezp8@miumg.edu.gt>
2025-09-05 11:31:21 -0600

    segundo commit

--- routes/usuariosPermisos.js
+++ routes/usuariosPermisos.js
@@ -6,6 +6,6 @@ const verificarToken = require('../middlewares/verificarToken'); // ✅ Importa 
 
 //router.post('/buscar/perfiles',verificarToken, usuariosController.buscarusuarioUbicacion);   // ✅ GET usuario por ID (ESTA ES LA CLAVE)
 router.post('/',verificarToken, usuariospermisosController.permisos);   // ✅ GET usuario por ID (ESTA ES LA CLAVE)
-router.post('/rol',verificarToken, usuariospermisosController.permisos);   // ✅ GET usuario por ID (ESTA ES LA CLAVE)
+router.post('/asignacionusuario',verificarToken, usuariospermisosController.usuarioRolinsert);   // ✅ GET usuario por ID (ESTA ES LA CLAVE)
 
 module.exports = router;
