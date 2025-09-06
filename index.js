6694b96275e65550a65a2e04e8c88fe4fb8f4ec6
Leopoldo Méndez Pérez <lmendezp8@miumg.edu.gt>
2025-09-06 07:27:44 -0600

    segundo commit

--- index.js
+++ index.js
@@ -3,15 +3,6 @@ require('dotenv').config();
 const cors = require('cors');  // Importa el módulo cors
 const app = express();
 const usuariosRoutes = require('./routes/usuarios');
-const categoriasRoutes = require('./routes/categoria');
-const subcategoriaRoutes = require('./routes/subcategoria');
-const categoria_subcategoria = require('./routes/categoria_subcategoria');
-const usuario_servicio = require('./routes/usuario_servicio');
-const perfil_usuario = require('./routes/perfil_usuario');
-const crearussubcat = require('./routes/registrar_usuario_cat_subcroutes');
-const subirimagen = require('./routes/imagenCloud'); // Aquí importas la ruta de imagen
-const horario = require('./routes/horario_route'); // Aquí importas la ruta de imagen
-const historial_favorito = require('./routes/favorito_historial_routes'); // Aquí importas la ruta de imagen
 const usuariospermisosController = require('./routes/usuariosPermisos'); // Aquí importas la ruta de imagen
 const menus = require('./routes/menus'); // Aquí importas la ruta de imagen
 const rolMenu = require('./routes/rolMenu'); // Aquí importas la ruta de imagen
@@ -28,18 +19,6 @@ app.use('/api/permisos', usuariospermisosController);
 // Rutas
 app.use('/api/menus', menus);
 app.use('/api/rol_menu', rolMenu); // expone /api/roles/... y /api/permisos/usuario/...
-
-
-app.use('/api', categoriasRoutes); // Ahora tu ruta final será /api/categorias
-app.use('/api/subcategoria', subcategoriaRoutes);
-app.use('/api/categoria_subcategoria', categoria_subcategoria);
-app.use('/api/usuario_servicio', usuario_servicio);
-app.use('/api/perfil_usuario', perfil_usuario);
-app.use('/api/', crearussubcat);
-// Usar las rutas para manejar las imágenes
-app.use('/api/imagen', subirimagen);// POST y PUT
-app.use('/api/horario', horario);
-app.use('/api/hi_fav', historial_favorito);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en puerto ${port}`);
